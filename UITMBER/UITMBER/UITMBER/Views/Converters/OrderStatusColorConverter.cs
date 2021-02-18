3b0aa9f255c805894fc27d95b01465534a388a9d
Marcin Lipa <marcin.lipa@onet.pl>
2021-02-18 21:20:41 +0100

    Update OrdersPage View
    OrdersPage view colors and fonts update
    add converter to color order status differently

---
+++ UITMBER/UITMBER/UITMBER/Views/Converters/OrderStatusColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using UITMBER.Enums;
+using Xamarin.Forms;
+
+namespace UITMBER.Views.Converters
+{
+    public class OrderStatusColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            try
+            { 
+                if (value != null && OrderStatus.Finished.Equals((OrderStatus)value))
+                    return "#941016";
+            }
+            catch (Exception)
+            { }
+            return "#bb171e";
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
